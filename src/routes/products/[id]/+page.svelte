b37d7443b9debe54edff1641e89cf72ee4ec54e4
yulahabogoslavets <test@test.email>
2025-03-07 20:50:29 +0100

    feat: add loading animation for detail

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -20,7 +20,38 @@
 
 <main class="container mx-auto flex flex-col gap-4 px-4 py-10">
 	{#if fetching}
-		<p>Loading...</p>
+		<section class="flex justify-between gap-4">
+			<!-- Lightbox Skeleton -->
+			<div class="skeleton" style="width: 200px; height: 200px; margin-right: 20px;"></div>
+
+			<div class="flex w-full flex-col gap-4">
+				<!-- Title Skeleton -->
+				<div class="skeleton skeleton-title skeleton-pulse"></div>
+
+				<!-- Rating & Reviews Skeleton -->
+				<div class="skeleton skeleton-pulse" style="width: 30%;"></div>
+				<div class="skeleton skeleton-pulse" style="width: 20%;"></div>
+
+				<!-- Price Skeleton -->
+				<div class="skeleton skeleton-price skeleton-pulse"></div>
+
+				<!-- Stock Availability Skeleton -->
+				<div class="skeleton skeleton-pulse" style="width: 40%;"></div>
+
+				<!-- Button Skeleton -->
+				<div class="skeleton skeleton-button skeleton-pulse"></div>
+
+				<!-- Description Skeleton -->
+				<div class="skeleton skeleton-pulse" style="width: 90%;"></div>
+				<div class="skeleton skeleton-pulse" style="width: 90%;"></div>
+			</div>
+		</section>
+
+		<section id="rewiews" class="mt-4 flex gap-4">
+			<div class="skeleton skeleton-title skeleton-pulse"></div>
+			<div class="skeleton skeleton-pulse" style="width: 60%;"></div>
+			<div class="skeleton skeleton-pulse" style="width: 80%;"></div>
+		</section>
 	{:else if product}
 		<section class="flex justify-between gap-4">
 			<Lightbox src={product.thumbnail} alt={product.title} />
@@ -73,3 +104,40 @@
 		<p>Product not found</p>
 	{/if}
 </main>
+
+<style>
+	.skeleton {
+		background-color: #e0e0e0;
+		border-radius: 4px;
+		height: 20px;
+		width: 100%;
+		margin-bottom: 10px;
+	}
+	.skeleton-title {
+		height: 30px;
+		width: 80%;
+	}
+
+	.skeleton-price {
+		height: 20px;
+		width: 50%;
+	}
+	.skeleton-button {
+		height: 40px;
+		width: 100%;
+	}
+	.skeleton-pulse {
+		animation: pulse 1.5s infinite ease-in-out;
+	}
+	@keyframes pulse {
+		0% {
+			background-color: #e0e0e0;
+		}
+		50% {
+			background-color: #c9c9c9;
+		}
+		100% {
+			background-color: #e0e0e0;
+		}
+	}
+</style>
